e1c8948dce6045f4a3387eb179d323823f6347d5
João Paulo Ferreira <paulojp.dev@gmail.com>
2024-04-06 21:45:46 -0300

    feat: create user model + test user inserts

---
+++ stateless-auth-api/src/main/resources/import.sql
@@ -0,0 +1,6 @@
+set schema 'public';
+
+-- password = 123123
+INSERT INTO "user" (id, username, password) VALUES (1, 'user.test', '$2a$10$1q8FHgW2qW1.ELy3rhrBcOAt/pMZ4xQQk2.NabTsw4bZuji9ofRWu');
+-- password = 123123
+INSERT INTO "user" (id, username, password) VALUES (2, 'user.test2', '$2a$10$1q8FHgW2qW1.ELy3rhrBcOAt/pMZ4xQQk2.NabTsw4bZuji9ofRWu');
