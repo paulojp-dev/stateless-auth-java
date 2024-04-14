7117bb06c4d6f32044169fc8b30296d24e2f112a
João Paulo Ferreira <paulojp.dev@gmail.com>
2024-04-14 06:48:00 -0300

    build: auto application build with script build.py

---
+++ stateless-auth-api/build.py
@@ -0,0 +1,45 @@
+import os
+import threading
+
+threads = []
+
+def build_application(app):
+    threads.append(app)
+    print("Building application {}".format(app))
+    os.system("./gradlew build".format(app))
+    print("Application {} finished building!".format(app))
+    threads.remove(app)
+
+
+def docker_compose_up():
+    print("Running containers!")
+    os.popen("docker-compose up --build -d").read()
+    print("Pipeline finished!")
+
+
+def build_all_applications():
+    print("Starting application build!")
+    threading.Thread(target=build_application,
+                     args={"stateless-auth-api"}).start()
+
+
+def remove_containers():
+    print("Removing containers.")
+    os.system("docker-compose down")
+    containers = os.popen('docker ps -a --format "{{.ID}}\t{{.Names}}" | grep "stateless-auth-" | cut -f1').read().split('\n')
+    containers.remove('')
+    if len(containers) > 0:
+        print("There are still {} containers created".format(containers))
+        for container in containers:
+            print("Stopping container {}".format(container))
+            os.system("docker container stop {}".format(container))
+        # os.system("docker container prune -f")
+
+
+if __name__ == "__main__":
+    print("Pipeline started!")
+    build_all_applications()
+    while len(threads) > 0:
+        pass
+    remove_containers()
+    threading.Thread(target=docker_compose_up).start()
